7208ea72fae9a5e145d10f73b25b0f065c08426f
Adriluminati <adri99rey3@gmail.com>
2020-11-04 21:20:55 +0100

    1.0

--- button.js
+++ button.js
@@ -1,17 +1,39 @@
+/*
 $(document).ready(function(){
-
-	$('.ir-arriba').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 500);
+	$('.goup').hide();
+	$('.goup').click(function(){
+		$('body,html').animate({
+			scrollTop:0
+		},1000)
 	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(300);
-		} else {
-			$('.ir-arriba').slideUp(300);
+	$(window).scroll(function () {
+		if ($(this).scrollTop() &gt; 200) {
+			$('.goup').fadeIn();
+		}
+		else {
+			$('.goup').fadeOut();
 		}
 	});
-
 });
+*/
+
+
+
+mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
