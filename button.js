bf43503dbb716e7d27cd6b50e33dcab2b0dd377f
Adriluminati <adri99rey3@gmail.com>
2020-11-08 14:42:41 +0100

    Button and alert

--- button.js
+++ button.js
@@ -1,39 +1,12 @@
-/*
-$(document).ready(function(){
-	$('.goup').hide();
-	$('.goup').click(function(){
-		$('body,html').animate({
-			scrollTop:0
-		},1000)
-	});
-	$(window).scroll(function () {
-		if ($(this).scrollTop() &gt; 200) {
-			$('.goup').fadeIn();
-		}
-		else {
-			$('.goup').fadeOut();
-		}
-	});
-});
-*/
-
-
-
-mybutton = document.getElementById("myBtn");
-
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+var mybutton = document.getElementById("myBtn");
 
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+
+function myFunction() {
+  alert("¡¡QUE NO PULSES EL @*#% BOTÓN!!");
 }
